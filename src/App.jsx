0627346d7fba7239c548a3fc513151af7c9e7424
JJQKR <yjcoyjco@naver.com>
2024-05-20 19:01:01 +0900

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ export default function App() {
     { name: "Charlie", age: 16, grade: "C" },
     { name: "Diana", age: 19, grade: "D" },
   ];
-
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((student) => student.age >= 18);
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
+        {filteredStudents.map((filteredStudent) => {
+          return (
+            <li
+              key={filteredStudent.name}
+              onClick={() => {
+                alert(filteredStudent.age, filteredStudent.grade);
+              }}
+            >
+              {filteredStudent.name}
+            </li>
+          );
+        })}
+
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
